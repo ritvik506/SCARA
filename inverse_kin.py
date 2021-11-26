1656fba98a6bd2976a1cae1ca611dd75112ef595
ritvik506 <43143348+ritvik506@users.noreply.github.com>
2021-11-26 15:49:52 +0530

    Delete inverse_kin.py

--- inverse_kin.py
+++
@@ -1,9 +0,0 @@
-import math
-
-x = 15
-y = 15
-
-theta1 = math.acos(x/(math.sqrt(x ** 2+y ** 2))) - \
-    math.acos((x ** 2+y ** 2+125)/(30*math.sqrt(x ** 2+y ** 2)))
-theta2 = math.acos((x-15*math.cos(theta1))/10)-theta1
-print(f"Theta1: {theta1*180/math.pi}\nTheta2: {theta2*180/math.pi}")
